937497195f687e60a1b11c6d4fb17add4b131fdf
msyk9038 <41169937+msyk9038@users.noreply.github.com>
2021-12-15 10:28:09 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+bluetooth.startUartService()
 basic.showNumber(5)
 basic.pause(200)
 basic.showNumber(4)
@@ -11,6 +12,7 @@ basic.pause(200)
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        bluetooth.uartWriteNumber(0)
         for (let index = 0; index < 25; index++) {
             pins.digitalWritePin(DigitalPin.P1, 1)
             basic.pause(100)
@@ -23,6 +25,8 @@ basic.forever(function () {
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
         pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P2, 0)
         basic.pause(5000)
     }
     pins.digitalWritePin(DigitalPin.P2, 0)
