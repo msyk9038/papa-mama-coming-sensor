d39cf652f29276156b831d72d2fe82236693779d
msyk9038 <41169937+msyk9038@users.noreply.github.com>
2021-12-15 10:35:49 +0900

    感知した回数を表示する機能を追加

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 bluetooth.startUartService()
+let 感知回数 = 0
 basic.showNumber(5)
 basic.pause(200)
 basic.showNumber(4)
@@ -9,7 +10,7 @@ basic.showNumber(2)
 basic.pause(200)
 basic.showNumber(1)
 basic.pause(200)
-basic.showIcon(IconNames.Heart)
+basic.showNumber(感知回数)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
         bluetooth.uartWriteNumber(0)
@@ -24,9 +25,14 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(100)
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        感知回数 += 1
+        for (let index = 0; index < 3; index++) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(100)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            basic.pause(100)
+        }
+        basic.showNumber(感知回数)
         basic.pause(5000)
     }
     pins.digitalWritePin(DigitalPin.P2, 0)
